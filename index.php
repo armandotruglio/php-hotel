d16ea3425d03689e2eca94403dde0f9452aa8944
armandotruglio <armando.truglio1@gmail.com>
2024-10-03 16:18:42 +0200

    Printed array content whit echo construct

--- index.php
+++ index.php
@@ -39,3 +39,35 @@ $hotels = [
     ],
 
 ];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($hotels as $hotel) { ?>
+        <li>
+            <?php echo $hotel["name"] ?>
+            <?php echo $hotel["description"] ?>
+            <?php echo $hotel["parking"] ?>
+            <?php echo $hotel["vote"] ?>
+            <?php echo $hotel["distance_to_center"] ?>
+        </li>
+        <?php } ?>
+    </ul>
+</body>
+
+</html>
